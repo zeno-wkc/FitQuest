d599bf4f848d7f59669fa69071131256ab890e85
zeno <zeno.bcit@gmail.com>
2024-04-28 22:07:09 -0700

    Update 3.0.14 by Zeno

--- pages/accountCreate.js
+++ pages/accountCreate.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "@/styles/AccountCreate.module.css";
+import styles from "@/styles/Accountcreate.module.css";
 import Image from "next/image";
 import { useState } from  "react";
 import fs from 'fs';
